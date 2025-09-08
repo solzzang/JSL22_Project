789b484991170c72d067bc981eae9fa00f9a9376
itak777 <ultraitak@gmail.com>
2025-09-08 17:14:22 +0900

    새로운 시작
    화이팅

---
+++ src/main/java/com/team/controller/CommunityController.java
@@ -0,0 +1,25 @@
+package com.team.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/community")
+public class CommunityController {
+    
+    @GetMapping("")
+    public String community() {
+        return "community/communityList"; // templates/community/list.html
+    }
+    
+    @GetMapping("/write")
+    public String write() {
+        return "community/communityWrite"; // templates/community/write.html
+    }
+    
+    @GetMapping("/detail")
+    public String communityDetail() {
+        return "community/communityDetail"; // templates/community/write.html
+    }
+}
---
+++ src/main/java/com/team/controller/MemberController.java
@@ -0,0 +1,33 @@
+package com.team.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+    
+    @GetMapping("/login")
+    public String login() {
+        return "member/login"; // templates/member/login.html
+    }
+    
+    @GetMapping("/signup")
+    public String register() {
+        return "member/signup"; // templates/member/register.html
+    }
+    
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "member/mypage"; // templates/member/mypage.html
+    }
+}
